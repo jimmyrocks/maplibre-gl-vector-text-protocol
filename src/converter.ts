98d425b421c1e1be2e18d35a620dd2a330aedf21
jimmyrocks <jim@loc8.us>
2023-04-09 14:09:33 +0000

    Add comments and clean up code a little

--- src/converter.ts
+++ src/converter.ts
@@ -7,12 +7,8 @@ import * as toGeoJson from '@tmcw/togeojson';
 export const supportedFormats = ['topojson', 'kml', 'gpx', 'tcx', 'csv', 'tsv'] as const;
 export type supportedFormatsType = typeof supportedFormats[number];
 
-export class Converter {
-    blankGeoJSON: () => FeatureCollection = () => ({
-        'type': 'FeatureCollection',
-        'features': []
-    });
 
+export class Converter {
     _conversionFn: () => Promise<FeatureCollection>;
     _rawData: string;
     _format: supportedFormatsType;
@@ -33,6 +29,15 @@ export class Converter {
 
     }
 
+    /**
+     * Creates a blank GeoJSON feature collection.
+     * @returns A new GeoJSON feature collection with no features.
+     */
+    blankGeoJSON: () => FeatureCollection = () => ({
+        type: 'FeatureCollection',
+        features: [],
+    });
+
     async convert() {
         if (!this._conversionFn) {
             return new Promise((_, rej) => rej(`No converter exists for ${this._format}`));
@@ -41,8 +46,12 @@ export class Converter {
         }
     }
 
+    /**
+     * Load the XML data as GeoJSON
+     * @returns A promise resolving to a GeoJSON FeatureCollection
+     */
     async loadXml(): Promise<FeatureCollection> {
-
+        // Use the appropriate parser based on the format
         const geojson = (toGeoJson as any)[this._format](
             new DOMParser().parseFromString(this._rawData, "text/xml")
         );
@@ -50,35 +59,47 @@ export class Converter {
         return geojson;
     }
 
+    /**
+     * Loads and parses CSV data into a GeoJSON FeatureCollection.
+     * @returns A Promise that resolves with the GeoJSON FeatureCollection.
+     */
     async loadCsv(): Promise<FeatureCollection> {
-
-        type csvOptions = any; //TODO
-        let geojson = this.blankGeoJSON();
-        let options: csvOptions = {};
+        // Define options for the csv2geojson library
+        let options: csv2geojson.csvOptions = {}; // TODO allow CSV options
 
         if (this._format === 'tsv') {
             options.delimiter = '\t';
         }
 
-        geojson = await new Promise((res, rej) => {
+        // Use the csv2geojson library to convert the CSV to GeoJSON
+        const geojson = await new Promise<FeatureCollection>((resolve, reject) => {
             csv2geojson.csv2geojson(
                 this._rawData,
                 options,
-                (err: string, data: FeatureCollection) => err ? rej(err) : res(data)
-            )
+                (err: string, data: FeatureCollection) => {
+                    if (err) {
+                        reject(err);
+                    } else {
+                        resolve(data);
+                    }
+                }
+            );
         });
 
         return geojson;
     }
 
+    /**
+     * Loads TopoJSON data and converts it into a GeoJSON FeatureCollection
+     */
     async loadTopoJson(): Promise<FeatureCollection> {
 
-        type topojson = {
+        type TopoJson = {
             type?: 'Topology';
             objects?: { [key: string]: Topology };
             arcs?: any;
         };
-        let topoJsonData: topojson = {};
+        let topoJsonData: TopoJson = {};
 
         try {
             topoJsonData = JSON.parse(this._rawData);
